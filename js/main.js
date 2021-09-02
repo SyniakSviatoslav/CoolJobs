c14d8ec7c54c81d27fd862d1b6db114069257b36
Syniak Sviatoslav <sandmann.holzmann@gmail.com>
2021-09-02 12:27:34 +0300

    edited js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     renderJobs();
     document.getElementById("add").addEventListener("click", createJob)
 });
@@ -9,12 +9,17 @@ let companyInput = document.getElementById("company-input");
 let descriptionInput = document.getElementById("description-input");
 let salaryInput = document.getElementById("salary-input")
 
-async function renderJobs (){
+{/* <input type="input" value="${item.name}" class="taskInput" id ="${item.id}" maxlength = "35"> */ }
+// myPara.setAttribute("id", "id_you_like");
+// var input = document.createElement("input");
+// input.type = "text";
+
+async function renderJobs() {
     const jobsData = await fetch("https://61293a58068adf001789b83c.mockapi.io/api/jobs");
     console.log(jobsData)
     let jobs = await jobsData.json();
-    jobs.forEach((job)=>{
-        let jobBlock1 = document.createElement("p");
+    jobs.forEach((job) => {
+        let jobBlock1 = document.createElement("input");
         let jobBlock2 = document.createElement("p");
         let jobBlock3 = document.createElement("p");
         let jobBlock4 = document.createElement("p");
@@ -22,13 +27,16 @@ async function renderJobs (){
         jobBlock2.classList.add('salary');
         jobBlock3.classList.add('company');
         jobBlock4.classList.add('description');
+        jobBlock1.setAttribute("id", job.title)
+
+
         let removeButton = document.createElement("button");
-        removeButton.addEventListener("click",removeJob);
+        removeButton.addEventListener("click", removeJob);
         removeButton.textContent = "REMOVE";
         removeButton.setAttribute("id", job.id)
         removeButton.classList.add('delete')
 
-        jobBlock1.textContent = `${job.title}`
+        jobBlock1.value = `${job.title}`
         jobBlock2.textContent = `${job.companyName}`
         jobBlock3.textContent = `${job.salary}`
         jobBlock4.textContent = `${job.description}`
@@ -40,7 +48,7 @@ async function renderJobs (){
     })
 }
 
-async function createJob(){
+async function createJob() {
     let job = {
         title: nameInput.value,
         companyName: companyInput.value,
@@ -52,11 +60,11 @@ async function createJob(){
 
 
     let response = await fetch('https://61293a58068adf001789b83c.mockapi.io/api/jobs', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(job)
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(job)
     });
 
 
@@ -64,14 +72,47 @@ async function createJob(){
 
 
 
-    // let result = await response.json();
-    // console.log(response)
+    let result = await response.json();
+    console.log(result.message)
 }
 
-async function removeJob(){
+async function removeJob() {
     let response = await fetch(`https://61293a58068adf001789b83c.mockapi.io/api/jobs/${this.id}`, {
         method: 'DELETE',
     });
     let result = await response.json();
     return result
 }
+
+const jobsData = fetch("https://61293a58068adf001789b83c.mockapi.io/api/jobs");
+console.log(jobsData)
+let oldJobs = jobsData;
+
+document.getElementsByTagName('input').forEach(item => {
+    item.addEventListener('input', event => {
+
+        let updatedJobs = oldJobs.reduce((acc, job) => {
+
+            return [...acc, +event.target.id === job.id ? {
+                ...job,
+                name: event.target.value
+
+            } : job]
+
+        }, [])
+        jobs = updatedJobs;
+
+    })
+    fetch(`https://61293a58068adf001789b83c.mockapi.io/api/jobs/${this.id}`, {
+        method: "PATCH",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            done: updatedJobs
+        })
+    })
+})
+
+
+//  
